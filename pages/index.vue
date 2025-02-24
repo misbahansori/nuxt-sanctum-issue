187538c99ceeabb4ac47904df7ee0db9eb73e54a
Misbah Ansori <misbah.ansori24@gmail.com>
2025-02-24 17:15:17 +0700

    wip

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const { data: users } = useSanctumFetch<any>("/api/users");
+</script>
+
+<template>
+  <div>
+    <h1>Users</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        <NuxtLink :to="`/user/${user.id}`"> {{ user.name }} </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ pages/user/[id].vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const route = useRoute();
+const { data: user } = await useLarafetch<any>(`/api/users/${route.params.id}`);
+
+console.log(user.value);
+
+if (!user.value) {
+  throw createError({
+    statusCode: 404,
+    message: "User not found",
+    fatal: true,
+  });
+}
+</script>
+
+<template>
+  <div>
+    <h1>User</h1>
+    <pre> {{ user }} </pre>
+  </div>
+</template>
